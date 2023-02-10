e6df058c4b800a374959bf133560bbb85165bce1
russell.cavalcante <russell.cavalcante@poncetech.com.br>
2023-02-10 14:01:17 -0300

    feito coment do server run

--- server.py
+++ server.py
@@ -2,4 +2,5 @@ from app import server, banco
 
 if __name__ == '__main__':
     # banco.init_app(server)
-    server.run(host = '0.0.0.0', debug=True)
+    # server.run(host = '0.0.0.0', debug=True)
+    server.run()
