df46d11c3dec1c395fa2b85cf42e389125d809d6
russell.cavalcante <russell.cavalcante@poncetech.com.br>
2023-02-16 07:53:17 -0300

    feito update no arquivo server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from app import server
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # banco.init_app(server)
     # server.run(host = '0.0.0.0', debug=True)
-    server.run(debug=False)
+server.run(debug=False)
