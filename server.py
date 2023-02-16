068807f9793d4b650104eb92816d670e5ed3d3db
russell.cavalcante <russell.cavalcante@poncetech.com.br>
2023-02-16 17:20:07 -0300

    feito alteração na string de conexão com o banco

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from app import server
 if __name__ == '__main__':
     # banco.init_app(server)
     # server.run(host = '0.0.0.0', debug=True)
-# server.run(debug=False)
-    port = int(os.environ.get("PORT", 5000))
-    server.run(host='0.0.0.0', port=port)
+
+    # port = int(os.environ.get("PORT", 5000))
+    # server.run(host='0.0.0.0', port=port)
+    server.run(debug=False)
