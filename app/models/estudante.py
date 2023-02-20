8dac6c3319bd95cebdfcfaf1cd376d80789c013a
russell.cavalcante <russell.cavalcante@poncetech.com.br>
2023-02-20 16:20:26 -0300

    feito crud de estudante

--- app/models/estudante.py
+++ app/models/estudante.py
@@ -51,14 +51,14 @@ class estudanteModel():
         return listEstadosDict
 
     @classmethod
-    def create_turma(*args, **kwargs):
+    def create_estudante(*args, **kwargs):
         # user = cls.query.filter_by(username=username).first()  #select * from hoteis where hotel_id = $hotel_id
         # try:
             cursor = conn.cursor()
             # print(args[1], args[2], args[3])
             # input()
             
-            cursor.execute("insert into turma ( cod_turma , FK_etapa_ensino_id , ano, FK_modalidade_id, FK_turno_id) values(?,?,?,?,?)",args[1], args[2], args[3], int(args[4]), int(args[5]))
+            cursor.execute("insert into estudante ( cod_nacional_estudante, nome , data_nascimento, FK_tipo_estudante_id, FK_etapa_ensino_id, ano, nee, nome_mae_aluno) values(?,?,?,?,?,?,?)",int(args[1]), args[2], args[3], int(args[4]), int(args[5]), args[6], args[7])
             
             conn.commit()
             # conn.close()
---
+++ app/services/estudante.py
@@ -0,0 +1,41 @@
+from flask_restful import Resource, reqparse
+from app.models.estudante import estudanteModel
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+# from app.config import conn
+
+# from werkzeug.security import safe_str_cmp
+from app.blacklist import BLACKLIST
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('cod_nacional_estudante', type=str, help="campo obrigatorio ")
+atributos.add_argument('nome', type=int, help="campo obrigatorio")
+atributos.add_argument('data_nascimento', type=str, help="campo obrigatorio ")
+atributos.add_argument('tipo_aluno_id', type=int, help="campo obrigatorio ")
+atributos.add_argument('etapa_ensino_id', type=int, help="campo obrigatorio ")
+atributos.add_argument('ano', type=str, help="campo obrigatorio ano ")
+atributos.add_argument('nee', type=str, help="campo obrigatorio nee ")
+atributos.add_argument('nome_mae_aluno', type=str, help="campo obrigatorio nome_mae_aluno")
+
+class GetEstudante(Resource):
+
+    @jwt_required()
+    def get(self, *args, **kwargs):
+        
+        return  estudanteModel.get_estudante(), 200
+
+    @jwt_required()
+    def post_estudante(self, *args, **kwargs):
+        dados = atributos.parse_args()
+        
+        cod_nacional_estudante = dados['cod_nacional_estudante'].strip()
+        nome = dados['nome'].strip()
+        data_nascimento = dados['data_nascimento'].strip()
+        tipo_aluno_id = dados['tipo_aluno_id'].strip()
+        ano = dados['ano'].strip()
+        nee = dados['nee']
+        nome_mae_aluno = dados['nome_mae_aluno'].strip()
+        
+        estudanteModel.create_estudante(cod_nacional_estudante ,nome, data_nascimento, tipo_aluno_id, ano, nee, nome_mae_aluno)
+        
+        return  {'created': nome}, 201
+    
