bb2144fb8a6a37a4fe7f228d7d4227b28cfd8587
russell.cavalcante <russell.cavalcante@poncetech.com.br>
2023-03-13 10:13:48 -0300

    feito atualização do gets de turma

--- app/models/turma.py
+++ app/models/turma.py
@@ -29,7 +29,7 @@ class TurmaModel():
     def get_turma(*args, **kwargs):
         cursor = conn.cursor()
  
-        cursor.execute("select * from turma;")
+        cursor.execute("select turma.id, turma.nome_turma, turma.ano_letivo, turma.FK_modalidade_id, turma.FK_grau_etapa_ensino_id, turma.FK_turno_id, escola.nome_escola, escola.FK_municipio_id, municipio.nome, municipio.FK_UF_id, estado.uf, estado.nome FROM turma INNER JOIN escola ON turma.FK_escola_id = escola.id INNER JOIN municipio ON escola.FK_municipio_id = municipio.id INNER JOIN estado ON municipio.FK_UF_id = estado.id")
         
         result = cursor.fetchall()
         cursor.close()
@@ -39,7 +39,18 @@ class TurmaModel():
         listEstadosDict = []
         for estadoTupla in result:
             
-            tup1 = ('id', 'cod_turma','FK_etapa_ensino_id', 'ano_letivo', 'FK_modalidade_id', 'FK_turno_id', 'FK_grau_etapa_ensino_id') 
+            tup1 = ('id',
+                    'nome_turma',
+                    'ano_letivo',
+                    'FK_modalidade_id',
+                    'FK_grau_etapa_ensino_id',
+                    'FK_turno_id',
+                    'nome_escola',
+                    'FK_municipio_id',
+                    'nome_municipio',
+                    'FK_UF_id',
+                    'uf',
+                    'nome_uf') 
             tup2 = estadoTupla
            
             if len(tup1) == len(tup2): 
